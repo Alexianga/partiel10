33ec3f92a51e573908f53500ef3a67848c9c23da
Alexianga <91941751+Alexianga@users.noreply.github.com>
2021-12-10 06:29:21 +0100

    Add files via upload

---
+++ connexion.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Connexion</title>
+        <meta charset="utf-8">
+</head>
+<body>
+     <form method="POST" action="" align="center">
+
+       <input type="text" name="speudo">
+       <br>
+       <input type="password" name="mdp">
+       <br><br>
+       <input type="submit" name="envoi">
+</form>
+
+</body>
+</html>
